fcd0e78326c2e128a5657b1ab42617cb94922740
yusuke-hub <yk1210140408@gmail.com>
2020-04-21 17:21:36 +0900

    Emmergent saved for reusing code

---
+++ MeditateApp/PlayViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PlayViewController.swift
+//  MeditateApp
+//
+//  Created by 川端雄介 on 2020/04/21.
+//  Copyright © 2020 Yusuke Kawabata. All rights reserved.
+//
+
+import UIKit
+
+class PlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
